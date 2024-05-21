768811151046a9ae619a6603ee32746e051da1bc
jovid_18 <cshcho99@gmail.com>
2024-05-21 21:56:10 +0900

    FEAT: init .env setting

--- sparta-nest/src/app.module.ts
+++ sparta-nest/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
+import { TypeOrmConfigService } from './config/typeorm.config.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [BoardModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // 일단 이것은 무조건 가장 위에서!
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // 주목
+      useClass: TypeOrmConfigService,
+      inject: [ConfigService], // 주목
+    }),
+    BoardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
